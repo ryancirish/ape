22cb1508a6a14fbed18062499e55d920f6891765
ryancirish <organized.ryanirish@gmail.com>
2021-03-01 23:56:41 -0500

    testing oauth flow for MB

---
+++ ape/pages/test2.vue
@@ -0,0 +1,69 @@
+<template>
+	<div id="entry_success">
+		<h1>{{ test }}</h1>
+	</div>
+</template>
+
+<script>
+	import axios from 'axios'
+	import qs from 'qs'
+	import { MoneyButtonClient } from '@moneybutton/api-client'
+export default {
+
+  name: 'test2',
+
+  data () {
+    return {
+   	  test: ''
+    }
+  },
+
+  async mounted() {
+
+  	let q = this.$route.query.code
+  	let s = this.$route.query.state
+  	let inj = {
+  		grant_type: 'authorization_code',
+  		client_id: process.env.NUXT_ENV_CLIENT_ID,
+  		code: q,
+  		redirect_uri: process.env.NUXT_ENV_CLIENT_REDIR
+  	}
+
+  	const config = {
+	  headers: {
+	    'Content-Type': 'application/x-www-form-urlencoded'
+	  }
+	}
+
+
+  	let z = await axios.post('https://www.moneybutton.com/oauth/v1/token', qs.stringify(inj))
+  	// handle error here
+  	this.test = z
+  // 		.then(response => {
+  // 			this.test = response.data
+  // 			console.log(response)
+  // 		})
+		// .catch((error) => {
+		//  console.log(error)
+		//  console.log('error ' + error);
+		// })
+  	// const AuthStr = 'Bearer '.concat(localStorage.getItem('mb_js_client:oauth_access_token'))
+  	
+  	// if (localStorage.getItem('mb_js_client:oauth_access_token')) {
+	  // 	axios.get('https://www.moneybutton.com/api/v1/auth/user_identity', { headers: { Authorization: AuthStr } })
+	  // 	 .then(response => {
+	  // 	     // If request is good...
+	  // 	     this.test = response.data
+	  // 	     console.log(response)
+
+	  // 	  })
+	  // 	 .catch((error) => {
+	  // 	     console.log('error ' + error);
+	  // 	  })
+  	// }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
